cf9d18cbd81218e2c7a2b052b607160af3737f64
Victor Gabriel <pvictorgabw@gmail.com>
2025-11-24 22:37:37 -0300

    Add files via upload

---
+++ static/scriptfile.js
@@ -0,0 +1,27 @@
+document.querySelectorAll("button")[4].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[5].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[6].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[7].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[8].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[9].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
+
+document.querySelectorAll("button")[10].addEventListener("click", function() {
+    document.body.classList.toggle("popup--aberto");
+})
